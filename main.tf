5f9aaff11c95894bbf600ce4383ab1fc94dc220d
Olivier <2535037+omoriceau@users.noreply.github.com>
2026-03-23 01:12:33 -0400

    Enable encryption for root block device

--- main.tf
+++ main.tf
@@ -50,6 +50,10 @@ resource "aws_instance" "web" {
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
+  root_block_device {
+    encrypted = true
+  }
+
   user_data = <<-EOF
               #!/bin/bash
               apt-get update
