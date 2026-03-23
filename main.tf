776a5310f258328b7a0fae7f2d1065e6638b1ca6
Olivier <2535037+omoriceau@users.noreply.github.com>
2026-03-23 01:16:41 -0400

    Refine security group rules in main.tf
    Updated ingress and egress rules to specify allowed IP.

--- main.tf
+++ main.tf
@@ -5,14 +5,14 @@ resource "aws_security_group" "web-sg" {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
-    # Restrict access to a specific IP or CIDR range
-    cidr_blocks = ["203.0.113.25/32"]
+    cidr_blocks = ["203.0.113.25/32"] # your specific allowed IP
   }
 
+  # restrict egress to only what is needed
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["203.0.113.25/32"] # or another specific destination
   }
 }
